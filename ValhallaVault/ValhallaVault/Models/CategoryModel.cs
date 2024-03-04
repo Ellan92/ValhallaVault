096acc65a51b2c25f5ccff01da4ea9352ce232a0
Ellan92 <elle9121@hotmail.se>
2024-03-04 15:14:06 +0100

    Copied from last failed project (teamwork)

---
+++ ValhallaVault/ValhallaVault/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValhallaVault.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
+        public List<SegmentModel> Segments { get; set; } = new();
+    }
+}
---
+++ ValhallaVault/ValhallaVault/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ValhallaVault.Data;
+
+namespace ValhallaVault.Models
+{
+    public class ResponseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsCorrect { get; set; }
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
