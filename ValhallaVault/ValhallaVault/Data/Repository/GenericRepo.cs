00d9b8399908f4036d01ed5d3e2c0723641bf2d6
emeliehensfelt <emelie046@gmail.com>
2024-03-05 11:16:37 +0100

    generiskt repo!

---
+++ ValhallaVault/ValhallaVault/Data/Repository/GenericRepo.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ValhallaVault.Data.Repository
+{
+    public class GenericRepo<T> where T : class
+    {
+        // Repo som har direktkontakt med databasen via dbcontext och returnerar data med hjälp av DbSet. 
+
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+
+        public GenericRepo(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        // Hitta model med ID
+        public T? GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        // Lägg till model
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            Complete();
+        }
+
+        // Delete model med ID
+        public void Delete(int id)
+        {
+            T? entityToDelete = GetById(id);
+
+            if (entityToDelete != null)
+            {
+                _dbSet.Remove(entityToDelete);
+            }
+        }
+
+        // Spara ändringar
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
