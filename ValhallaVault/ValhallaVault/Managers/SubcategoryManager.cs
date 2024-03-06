52c3ab8af46b9666c668ea23a25a8ad753f243bb
Farre <farrezzz@gmail.com>
2024-03-06 18:10:12 +0100

    Segmentpage, repo, manager

--- ValhallaVault/ValhallaVault/Managers/SubcategoryManager.cs
+++ ValhallaVault/ValhallaVault/Managers/SubcategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using ValhallaVault.Data.Repository;
+using ValhallaVault.Models;
 
 namespace ValhallaVault.Managers
 {
@@ -10,6 +11,12 @@ namespace ValhallaVault.Managers
         {
             _subcategoryRepo = subcategoryRepo;
         }
+        public async Task<List<SubcategoryModel>> GetSubcategoriesByCategoryIdAsync(int subcategoryId)
+        {
+            var segments = await _subcategoryRepo.GetSubcategoryBySegmentIdAsync(subcategoryId);
+            return segments;
+        }
+
         public async Task UpdateSubcategoryDescriptionAsync(int subcategoryId, string newDescription)
         {
             await _subcategoryRepo.UpdateSubcategoryDescriptionAsync(subcategoryId, newDescription);
