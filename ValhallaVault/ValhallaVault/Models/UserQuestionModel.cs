2afcf5134f6355afb20052490177e1dcd937f4e9
Broscan <Calle992015@gmail.com>
2024-03-06 15:39:15 +0100

    added new model

---
+++ ValhallaVault/ValhallaVault/Models/UserQuestionModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValhallaVault.Data;
+
+namespace ValhallaVault.Models
+{
+    [PrimaryKey("UserId", new string[] { "QuestionId" })]
+    public class UserQuestionModel
+    {
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+        public int QuestionId { get; set; }
+        public QuestionModel Question { get; set; } = null!;
+        public int ResponseId { get; set; }
+        public ResponseModel Response { get; set; } = null!;
+    }
+}
