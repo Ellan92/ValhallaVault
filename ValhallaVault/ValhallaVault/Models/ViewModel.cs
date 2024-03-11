6f341c9c414f71892574ebdba44a96e9e60fa7c5
Farre <farrezzz@gmail.com>
2024-03-11 11:26:57 +0100

    ViewModel added

---
+++ ValhallaVault/ValhallaVault/Models/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValhallaVault.Models
+{
+    public class ViewModel
+    {
+        public int Id { get; set; }
+        public bool? IsAnswered { get; set; }
+        public string? Question { get; set; }
+        public List<string>? Options { get; set; }
+        public SolutionModel Solution { get; set; } = null!;
+        // one-to-one med Solutionmodel
+        public int SubcategoryId { get; set; }  // one-to-many med SubcategoryModel
+        public SubcategoryModel SubCategory { get; set; }
+        // one-to-many med SubcategoryModel
+        public List<ResultModel> Results { get; set; } // one-to-many med Resultmodel
+    }
+}
