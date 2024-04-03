0de956198a37fc8b1c97ca30f2373e72ed6cbd13
emeliehensfelt <emelie046@gmail.com>
2024-04-03 17:12:38 +0200

    lagt till integrationstester

--- ValhallaVault/ValhallaVault/Managers/CategoryManager.cs
+++ ValhallaVault/ValhallaVault/Managers/CategoryManager.cs
@@ -9,15 +9,57 @@ namespace ValhallaVault.Managers
         {
             _categoryRepo = categoryRepo;
         }
+
         public async Task UpdateCategoryDescriptionAsync(int categoryId, string newDescription)
         {
-            await _categoryRepo.UpdateCategoryDescriptionAsync(categoryId, newDescription);
+            try
+            {
+                await _categoryRepo.UpdateCategoryDescriptionAsync(categoryId, newDescription);
+            }
+            catch (Exception ex)
+            {
+                // Anpassa här hur du vill hantera exceptionen när kategorin inte hittas
+                throw new Exception("Failed to update category description.", ex);
+            }
         }
 
         public async Task UpdateCategoryNameAsync(int categoryId, string newCategoryName)
         {
-            await _categoryRepo.UpdateCategoryNameAsync(categoryId, newCategoryName);
+            try
+            {
+                await _categoryRepo.UpdateCategoryNameAsync(categoryId, newCategoryName);
+            }
+            catch (Exception ex)
+            {
+                // Anpassa här hur du vill hantera exceptionen när kategorin inte hittas
+                throw new Exception("Failed to update category name.", ex);
+            }
+        }
+
+        public virtual async Task VirtualUpdateCategoryDescriptionAsync(int categoryId, string newDescription)
+        {
+            try
+            {
+                await _categoryRepo.VirtualUpdateCategoryDescriptionAsync(categoryId, newDescription);
+            }
+            catch (Exception ex)
+            {
+                // Anpassa här hur du vill hantera exceptionen när kategorin inte hittas
+                throw new Exception("Failed to update category description.", ex);
+            }
+        }
+
+        public virtual async Task VirtualUpdateCategoryNameAsync(int categoryId, string newCategoryName)
+        {
+            try
+            {
+                await _categoryRepo.VirtualUpdateCategoryNameAsync(categoryId, newCategoryName);
+            }
+            catch (Exception ex)
+            {
+                // Anpassa här hur du vill hantera exceptionen när kategorin inte hittas
+                throw new Exception("Failed to update category name.", ex);
+            }
         }
     }
 }
-
